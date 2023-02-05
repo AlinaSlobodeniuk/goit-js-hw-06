0e2903e3296f72b774e5dc864deba5081279cf95
Alina Slobodeniuk <akulalina25@gmail.com>
2023-02-06 01:02:37 +0200

    1task

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,23 @@
-const categoriesList = document.querySelector('#categories');
+const categoriesList = document.querySelector("#categories");
+const categoryElements = categoriesList.querySelectorAll(".item");
+console.log(`Number of categories: ${categoryElements.length}`);
+
+
+categoryElements.forEach((item) => {
+console.log(`Category: ${item.firstElementChild.textContent}`);
+
+console.log(`Elements: ${item.lastElementChild.children.length}`)
+})
+
+
+
+
+
+
+
+
+
+// categoryElements.forEach((item) => {
+//     console.log(`Category: ${item.firstElementChild.textContent}`);
+//     console.log(`Elements: ${item.lastElementChild.children.length}`)
+// });
