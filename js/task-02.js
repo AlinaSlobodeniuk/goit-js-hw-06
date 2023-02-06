cc01617ffd648381dfe2a414a5ce8f3027fa3e94
Alina Slobodeniuk <akulalina25@gmail.com>
2023-02-07 00:18:39 +0200

    2task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+// const list = document.querySelector("#ingredients");
+// ingredients.forEach((ingredient) => {
+//   const item = document.createElement("li");
+//   item.textContent = ingredient;
+//   item.classList.add(item);
+//   createList.append(item);
+//   console.log(item);
+// });
+
+const list = document.querySelector('#ingredients');
+
+let createElement = new DocumentFragment();
+
+for (const value of ingredients) {
+  const item = document.createElement('li');
+  item.classList.add('item');
+  item.textContent += value;
+  createElement.appendChild(item);
+}
+
+document.querySelector('ul#ingredients').append(createElement);
